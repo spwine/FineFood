1c09aba33d5e421192a759c7196d280895590792
spwine <101738450+spwine@users.noreply.github.com>
2022-03-28 11:50:49 +0700

    Add files via upload

--- form.php
+++ form.php
@@ -7,13 +7,16 @@ $message= $_POST['message'];
 $company= $_POST['companyName'];
 
 $to = "pongsakorn.wine@hotmail.com";
-$subject = "Mail From FineFoodservice";
-$txt ="Name = ". $name . "\r\n Company name = " . $company . "\r\n Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$subject = "Mail From FineFoodservice Contact form";
+$txt ="Name = $name \n".
+        "Company name = $company \n" .  
+            "Email = $email \n" .  
+                "Message = $message \n";
+$headers = "From: $email \r\n"
 //"CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
-    header("Location:thankyou.html");
+    header("Location:contact page.html");
 }
 //redirect
 
